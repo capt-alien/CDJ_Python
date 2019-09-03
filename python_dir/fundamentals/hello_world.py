bae85e9700b2f512d6f7ad5697bf95ac1e691ec2
capt-alien <eric.botcher@presidio.edu>
2019-09-03 11:29:39 -0700

    hello world

---
+++ python_dir/fundamentals/hello_world.py
@@ -0,0 +1,25 @@
+#!python3
+
+hw="Hello World!"
+
+
+# Variables
+myself="Captain Alien!"
+num = 12
+food_1="rice"
+food_2="BBQ"
+
+
+def main():
+    print(hw)
+    print("Hello ",myself)
+    print(f"Hello {myself}")
+    print("Hello ", num)
+    print(f"Hello {num}")
+    print("I like to eat ",food_1, "and ", food_2)
+    print(f"I like to eat {food_1} and {food_2}")
+
+
+# Exicutable code
+if __name__ == '__main__':
+    main()
