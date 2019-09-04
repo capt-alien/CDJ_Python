293a191de9d5de81ea917a2814d7780014dabf7c
capt-alien <eric.botcher@presidio.edu>
2019-09-04 10:49:10 -0700

    bank account

--- python_dir/oop/user.py
+++ python_dir/oop/user.py
@@ -7,42 +7,63 @@ class User:
     def __init__(self, name, starting_bal=500):
         self.name = name
         self.balance = starting_bal
+        # print("user: ", self.name, "created!")
         # self.password = password
 
     def make_deposit(self, amount):
         self.balance += amount
+        return self
 
     def make_withdrawal(self, amount):
         self.balance -= amount
+        return self
 
     def display_balance(self):
         print(self.name,": ", self.balance)
-        return self.balance
 
     def transfer(self, other_user, amount):
         self.make_withdrawal(amount)
         other_user.balance += amount
+        return self
 
+
+class BankAccount:
+    def __init__(self, int_rate=.0125, balance=500):
+        self.int_rate = int_rate
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        self.balance -= amount
+        return self
+
+    def display_account_info(self):
+        print("Balance: $", self.balance)
+
+    def yield_interest(self):
+        if self.balance > 0:
+            self.balance = self.balance*(1+ self.int_rate)
+        return self
 
 
 def main():
-    alex=User("alex")
-    jack=User("jack")
-    jill=User("jill")
-    alex.make_deposit(100)
-    alex.make_deposit(100)
-    alex.display_balance()
-    jack.make_deposit(300)
-    jack.make_withdrawal(50)
-    jack.make_withdrawal(75)
-    jack.display_balance()
-    jill.make_withdrawal(50)
-    jill.make_withdrawal(55)
-    jill.make_withdrawal(30)
-    jill.display_balance()
-    alex.transfer(jill, 100)
-    alex.display_balance()
-    jill.display_balance()
+    # alex=User("alex")
+    # alex.make_deposit(100).make_deposit(200).display_balance()
+    #
+    # jack=User("jack")
+    # jack.make_deposit(300).make_withdrawal(50).make_withdrawal(75).display_balance()
+    #
+    # jill=User("jill")
+    # jill.make_withdrawal(50).make_withdrawal(55).make_withdrawal(30).display_balance()
+    #
+    # alex.transfer(jill, 100).display_balance()
+    # jill.display_balance()
+
+    account_a = BankAccount(.030, 600).deposit(200).deposit(300).withdraw(100).display_account_info()
+    account_b = BankAccount().deposit(200).withdraw(100).withdraw(50).withdraw(50).display_account_info()
 
 
 
