6598e7e7d35d442833d5f5b9e0ad226aae2c87f7
capt-alien <eric.botcher@presidio.edu>
2019-09-06 12:51:53 -0700

    working template dojo

--- flask_dir/flask_fun/dojo_survey/app.py
+++ flask_dir/flask_fun/dojo_survey/app.py
@@ -14,16 +14,7 @@ def index():
 def save_survey():
     print("*"*50)
     print(request.form)
-
-    return render_template('result.html',name=request.form["name"], location=request.form["location"], language=request.form['language'], comment=request.form('comment') )
-
-
-# @app.route('/result')
-# def result():
-#     return render_template('result.html')
-#
-
-
+    return render_template('result.html',name=request.form["name"], location=request.form["location"], language=request.form["language"], comment=request.form["comment"] )
 
 #bottomware
 if __name__ == '__main__':
