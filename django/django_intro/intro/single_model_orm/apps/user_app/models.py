67b0cdbdaea2099a17a66f8ca841a9fb30e9d3bd
capt-alien <eric.botcher@presidio.edu>
2019-09-12 08:40:06 -0700

    finished user model

--- django/django_intro/intro/single_model_orm/apps/user_app/models.py
+++ django/django_intro/intro/single_model_orm/apps/user_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_adress= models.CharField(max_length=255)
+    age = models.IntegerField()
+    created_at= models.DateTimeField(auto_now=True)
+    updated_at= models.DateTimeField(auto_now=True)
