2bcea87adeb5963b9565d57ca15d2551d8c06eca
capt-alien <eric.botcher@presidio.edu>
2019-09-13 15:27:38 -0700

    someting

--- django/django_intro/intro/book_authors_proj/apps/books_authors_app/views.py
+++ django/django_intro/intro/book_authors_proj/apps/books_authors_app/views.py
@@ -4,4 +4,20 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Fucking Yeay!!!")
+    #needs to querry all books in the DB and display on the right of the screen
+    #
+    return render(request, "books_authors_app/index.html")
+
+
+def process_book(request):
+    pass
+
+
+
+def view_authors(request):
+    pass
+
+
+
+def submitt_new_author(request):
+    pass
