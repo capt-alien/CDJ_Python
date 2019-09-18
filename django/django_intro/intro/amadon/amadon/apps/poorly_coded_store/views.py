fbe2d462e42391cbc152869c8e77a469a585bdd9
capt-alien <eric.botcher@presidio.edu>
2019-09-18 09:46:25 -0700

    something

--- django/django_intro/intro/amadon/amadon/apps/poorly_coded_store/views.py
+++ django/django_intro/intro/amadon/amadon/apps/poorly_coded_store/views.py
@@ -11,6 +11,8 @@ def checkout(request):
     quantity_from_form = int(request.POST["quantity"])
     price_from_form = float(request.POST["price"])
     total_charge = quantity_from_form * price_from_form
+    print("*"*25)
+    print(total_charge)
     print("Charging credit card...")
     Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    return render(request, "store/checkout.html", total_charge)
